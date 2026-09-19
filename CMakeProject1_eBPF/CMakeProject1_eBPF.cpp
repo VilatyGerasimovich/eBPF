7c15f46493f252dec0b96a237bbc6eba0f8e69ea
Vitaly <gera.vetal.26@mail.ru>
2026-09-19 20:45:46 +0300

    4 commit

---
+++ CMakeProject1_eBPF/CMakeProject1_eBPF.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeProject1_eBPF.cpp: определяет точку входа для приложения.
+//
+
+//#include "CMakeProject1_eBPF.h"
+//#include <libbpf>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
